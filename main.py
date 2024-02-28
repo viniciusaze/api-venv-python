f0b5320c700e49d7ffe051fcfb52ebe79b247237
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-02-28 17:38:14 -0300

    aula sobre endpoints

---
+++ main.py
@@ -0,0 +1,44 @@
+#Começo e criação da rota
+
+from fastapi import FastAPI, Query
+import requests
+
+app = FastAPI()
+
+#Criando endpoint
+
+@app.get('/api/hello')
+def hello_world():
+    '''
+    
+    Endpoint que exibe uma mensagem no mundo da programação
+
+    '''
+    return {'Hello':'Word'}
+
+@app.get('/api/restaurantes/')
+def get_restaurantes(restaurante: str = Query(None)):
+    '''
+    
+    Endpoint para exibir o cardápio dos restaurantes
+
+    '''
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'Dados': dados_json}
+        dados_restaurante = []
+        for item in dados_json:
+            if item['Company'] == restaurante:
+                dados_restaurante.append({
+                    "item": item['Item'],
+                    "price": item['price'],
+                    "description": item['description']
+                }) 
+        return {'Restaurante': restaurante, 'Cardápio': dados_restaurante}
+
+    else: 
+        return {'Erro':f'{response.status_code} - {response.text}'}
